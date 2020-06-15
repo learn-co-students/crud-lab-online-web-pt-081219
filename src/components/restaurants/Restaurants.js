68d9cefbfa24e13475238eb637ee9af6b12e6c78
helloimiskra <helloimiskra@gmail.com>
2020-06-15 23:53:50 +0900

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant'
+
 
 class Restaurants extends Component {
+  renderRestaurants = () => {
+    return this.props.restaurants.map(r => <Restaurant key={r.id} restaurant={r} deleteRestaurant={this.props.deleteRestaurant}/> )
+}
   render() {
     return(
       <ul>
-        Restaurants Component
+        {this.renderRestaurants()}
       </ul>
     );
   }
 };
+
+
 
 export default Restaurants;
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+  
+  
+  renderReviews = () => {
+
+    return this.props.reviews.filter(r => r.restaurantId === this.props.restaurantId).map(r => <Review key={r.id} review={r} deleteReview={this.props.deleteReview}/> )
+  }
+  
+  
   render() {
+    
     return (
       <ul>
-        Reviews
+        {this.renderReviews()}
       </ul>
     );
   }
